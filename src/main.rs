0f72b87249caa045fa3462ea705a3c6d79c02065
haivo <hai.vo@parcelperform.com>
2023-09-20 13:08:37 +0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use druid::widget::{Button, Container, Flex, Label, LensWrap, List, ViewSwitcher
 use druid::{AppLauncher, PlatformError, Widget, WidgetExt, WindowDesc};
 use druid::{Data, Lens};
 use im::{vector, Vector};
+use std::sync::{Arc, Mutex};
 
-#[derive(Clone, Data, Lens)]
+#[derive(Clone, Data, Lens, Debug)]
 struct AppState {
-    items: Vector<String>,
+    items: Arc<Mutex<Vector<String>>>,
 }
 
 fn main() -> Result<(), PlatformError> {
@@ -16,7 +17,7 @@ fn main() -> Result<(), PlatformError> {
     //let data = 0_u32;
 
     let init_state = AppState {
-        items: vector!["".to_string()],
+        items: Arc::new(Mutex::new(vector!["".to_string()])),
     };
 
     AppLauncher::with_window(main_window).launch(init_state)
@@ -27,24 +28,26 @@ fn ui_builder() -> impl Widget<AppState> {
     let label = Label::new("Clipboard list").padding(5.0).center();
 
     // Dynamically create a list of buttons, one for each clipboard.
-    let list = LensWrap::new(
+    let list = Container::new(LensWrap::new(
         List::new(|| {
-            Container::new(
-                Flex::column().with_child(
-                    Label::dynamic(|item: &String, _env: &_| format!("Item: {}", item))
-                        .expand_width()
-                        .padding(5.0),
-                ),
-            )
+            Label::dynamic(|data, _env: &_| {
+                let item = data.items.lock().unwrap().clone();
+                let mut x = String::new();
+                item.iter().for_each(|i| x.push_str(i));
+                format!("Item: {}", x)
+            })
         }),
         AppState::items,
-    );
+    ));
 
     let button2 = Button::new("Store clipboard")
         .on_click(|_ctx, clip: &mut AppState, _env| {
             let mut clipboard = Clipboard::new().unwrap();
             println!("Clipboard: {}", clipboard.get_text().unwrap());
-            clip.items.push_back(clipboard.get_text().unwrap());
+            clip.items
+                .lock()
+                .unwrap()
+                .push_back(clipboard.get_text().unwrap());
         })
         .padding(5.0);
 
@@ -56,16 +59,14 @@ fn ui_builder() -> impl Widget<AppState> {
 
     let button3 = Button::new("Clear")
         .on_click(|_ctx, data: &mut AppState, _env| {
-            data.items.remove(0);
+            data.items.lock().unwrap().remove(0);
         })
         .padding(5.0);
 
-    Container::new(
-        Flex::column()
-            .with_child(label)
-            .with_child(button2)
-            .with_child(button)
-            .with_child(button3)
-            .with_child(list),
-    )
+    Flex::column()
+        .with_child(label)
+        .with_child(button2)
+        .with_child(button)
+        .with_child(list)
+        .with_child(button3)
 }
